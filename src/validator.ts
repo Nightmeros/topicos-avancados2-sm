e3ba47efa10839d3525359a510e38865f7ad6031
GUILHERME PEREIRA FRANCO <guilherme.franco005@cs.cruzeirodosul.edu.br>
2026-08-19 21:09:29 -0300

    correção no teste de minimo e maximo de caracteres

--- src/validator.ts
+++ src/validator.ts
@@ -3,12 +3,15 @@ function validarChamado(titulo: string): boolean {
         throw new Error("O título do chamado não pode ser vazio.");
     }
 
-    if(titulo.length<=5){
-        "o titulo deve ter no minimo 5 caracteres"
+    if (titulo.length <= 5) {
+        throw new Error("o titulo deve ter no minimo 5 caracteres");
+
     }
 
-    if(titulo.length>100){
-        "o titulo deve ter no maximo 100 caracteres"
+    if (titulo.length > 100) {
+        throw new Error("o titulo deve ter no maximo 100 caracteres");
+
+
     }
 
     return true;
